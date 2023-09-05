9ed6d60d3cf816dcca75ca978df00981203a8698
shuto kikai <yysskikai04@icloud.com>
2023-09-05 15:42:26 +0900

    warnningの修正とdestroyの追加

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,4 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
 
   def index
     @agendas = Agenda.all
@@ -15,17 +14,23 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
   end
 
-  private
-
-  def set_agenda
-    @agenda = Agenda.find(params[:id])
+  def destroy
+    @agenda = Agenda.find(params[:id]) # アジェンダのIDを取得してそれに紐付いているユーザーIDとチームのオーナーIDを持ってくる
+    if current_user.id != @agenda.user_id || current_user.id != @agenda.team.owner_id # 現在ログインしているユーザーIDがアジェンダの作成者IDとチームのオーナーIDの両方で一致しない場合
+      redirect_to dashboard_path, notice: "アジェンダの削除権限がありません"
+    else
+      @agenda.destroy
+      redirect_to dashboard_path notice: "アジェンダを削除しました"
+    end
   end
+
+  private
 
   def agenda_params
     params.fetch(:agenda, {}).permit %i[title description]
